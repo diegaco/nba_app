b7efef72b362827c27fb1b18568371da70530b92
diegaco <diegoacosta211@gmail.com>
2018-11-22 23:16:43 -0300

    adding some routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./components/Home";
+import Layout from "./hoc/Layout";
 
 class Routes extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Layout>
     );
   }
 }
