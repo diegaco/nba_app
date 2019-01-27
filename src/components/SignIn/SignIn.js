6b5c93d0a116b0bf6dbc14fe6dfd09d831a435de
diegaco <diegoacosta211@gmail.com>
2019-01-27 16:19:12 -0300

    add login form validations

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -46,20 +46,98 @@ class SignIn extends Component {
     const newFormData = {
       ...this.state.formData
     };
+
     const newElement = {
       ...newFormData[element.id]
     };
+
     newElement.value = element.event.target.value;
+
+    if (element.blur) {
+      let validData = this.validate(newElement);
+      newElement.valid = validData[0];
+      newElement.validationMessage = validData[1];
+    }
+
+    newElement.touched = element.blur;
     newFormData[element.id] = newElement;
+
     this.setState({
       formData: newFormData
     });
   };
 
+  validate = element => {
+    let error = [true, ""];
+
+    // validate email
+    if (element.validation.email) {
+      const valid = /\S+@\S+\.\S+/.test(element.value);
+      const msg = `${!valid ? "Must enter a valid email" : ""}`;
+      error = !valid ? [valid, msg] : error;
+    }
+
+    // validate password
+    if (element.validation.password) {
+      const valid = element.value.length >= 5;
+      const msg = `${!valid ? "Must be greater than 5" : ""}`;
+      error = !valid ? [valid, msg] : error;
+    }
+
+    // validate required
+    if (element.validation.required) {
+      const valid = element.value.trim() !== "";
+      const msg = `${!valid ? "This field is required" : ""}`;
+      error = !valid ? [valid, msg] : error;
+    }
+    return error;
+  };
+
+  submitButton = () =>
+    this.state.loading ? (
+      "loading..."
+    ) : (
+      <div>
+        <button onClick={event => this.submitForm(event, false)}>
+          Register Now
+        </button>
+        <button onClick={event => this.submitForm(event, true)}>Log in</button>
+      </div>
+    );
+
+  submitForm = (event, type) => {
+    event.preventDefault();
+    if (type !== null) {
+      let dataToSubmit = {};
+      let formIsValid = true;
+
+      for (let key in this.state.formData) {
+        dataToSubmit[key] = this.state.formData[key].value;
+      }
+
+      for (let key in this.state.formData) {
+        formIsValid = this.state.formData[key].valid && formIsValid;
+      }
+
+      if (formIsValid) {
+        this.setState({
+          loading: true,
+          registerError: ""
+        });
+
+        if (type) {
+          console.log("Login");
+        } else {
+          console.log("Register Now");
+        }
+      }
+    }
+  };
+
   render() {
     return (
       <div className={styles.LoginContainer}>
-        <form action="">
+        <form action="" onSubmit={event => this.submitForm(event, null)}>
           <h2>Register / Log In</h2>
           <FormField
             id={"email"}
@@ -72,6 +150,8 @@ class SignIn extends Component {
             formData={this.state.formData.password}
             handleChange={element => this.handleFormChange(element)}
           />
+
+          {this.submitButton()}
         </form>
       </div>
     );
--- src/components/widgets/FormFields/FormFields.js
+++ src/components/widgets/FormFields/FormFields.js
@@ -1,9 +1,21 @@
 import React from "react";
-// import styles from "./FormFields.module.css";
+import styles from "./FormFields.module.css";
 
 const FormFields = ({ formData, handleChange, id }) => {
   const renderTemplate = () => {
     let formTemplate = null;
+
+    const showError = () => {
+      let errorMessage = null;
+
+      if (formData.validation && !formData.valid) {
+        errorMessage = (
+          <div className={styles.LabelError}>{formData.validationMessage}</div>
+        );
+      }
+
+      return errorMessage;
+    };
 
     switch (formData.element) {
       case "input":
@@ -15,6 +27,7 @@ const FormFields = ({ formData, handleChange, id }) => {
               onBlur={event => handleChange({ event, id, blur: true })}
               onChange={event => handleChange({ event, id, blur: false })}
             />
+            {showError()}
           </div>
         );
         break;
